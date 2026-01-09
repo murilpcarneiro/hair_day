e11ee8cc86ca75280919f3a8b74a2e2dcef06b9a
Murilo <murilpcarneiro@gmail.com>
2026-01-09 01:08:47 -0300

    feat: implement form submission handling with client name and selected hour validation

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 
 const form = document.querySelector('form')
+const clientName = document.getElementById('client')
 const selectedDate = document.getElementById('date')
 
 // Data atual para formatar o input
@@ -13,5 +14,31 @@ selectedDate.min = inputToday
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log('Formulário enviado!')
+  try {
+    // Recuperando o nome do cliente
+    const name = clientName.value.trim()
+    if (!name) {
+      return alert('Informe o nome do cliente.')
+    }
+
+    // Recupera o horário selecionado
+    const selectedHour = document.querySelector('.hour-selected')
+    if (!selectedHour) {
+      return alert('Selecione um horário para o agendamento.')
+    }
+
+    // Recuperar somente a hora
+    const [hour] = selectedHour.innerText.split(':')
+
+    // Insere a hora na data
+    const when = dayjs(selectedDate.value).add(hour, 'hour')
+
+    // Gera um id
+    const id = new Date().getTime()
+
+    console.log({ id, name, when: when.format('YYYY-MM-DD HH:mm:ss') })
+  } catch (error) {
+    alert('Não foi possível realizar o agendamento.')
+    console.error(error)
+  }
 }
