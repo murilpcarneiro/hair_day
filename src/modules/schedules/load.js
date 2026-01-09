b9a63af03057b24fb0e7fe42dd0a2ffe4ac5089b
Murilo <murilpcarneiro@email.com>
2026-01-09 18:57:30 -0300

    feat: adiciona a funcionalidade de bucar os agendamentos do dia selecionado

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,17 @@
+import { scheduleFetchByDay } from '../../services/schedule-fetch-by-day.js'
 import { hoursLoad } from '../form/hours-load.js'
 
 // Seleciona o input de data
 const selectedDate = document.getElementById('date')
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtem a data selecionada no input
   const date = selectedDate.value
+
+  // Busca na API os agendamentos do dia selecionado
+  const dailySchedules = await scheduleFetchByDay({ date })
+
+  console.log(dailySchedules)
 
   // Renderiza as horas disponiveis.
   hoursLoad({ date })
--- src/services/schedule-fetch-by-day.js
+++ src/services/schedule-fetch-by-day.js
@@ -3,13 +3,13 @@ import { apiConfig } from './api-config'
 
 export async function scheduleFetchByDay({ date }) {
   try {
-    const response = await fetch(`${apiConfig.baseURL}/schedules}`)
+    const response = await fetch(`${apiConfig.baseURL}/schedules`)
 
     const data = await response.json()
 
-    const dailySchedules = data.filter((schedule) => {
+    const dailySchedules = data.filter((schedule) =>
       dayjs(date).isSame(schedule.when, 'day')
-    })
+    )
 
     return dailySchedules
   } catch (error) {
